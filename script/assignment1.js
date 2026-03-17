8f15f6b114348611f1479ce2e9fa8109dc5568d4
Anitha-Thunga <ani.cvs@gmail.com>
2026-03-17 10:02:28 +0100

    added java script code for display a simple message, to create & assign value to 3 different variable, to perform arithmatic oprations and display the results on console

---
+++ script/assignment1.js
@@ -0,0 +1,18 @@
+console.log("Hello wolrd");//to display Hello world on console
+ let name="Anitha";// name variable creation
+ console.log(`My name is:${name}`);// displays name with the message in the console
+let age=34;//age variable creation
+ console.log(`My age is: ${age}`);// displays age with the message in the console
+ let favoriteColor="red";//favorite color variable creation
+console.log(`My favorite color is:${favoriteColor}`);// displays favorite color with the message in the console
+
+let x=120;// create and assign value to variable x
+let y=350;// create and assign value to variable y
+z=x+y;// add x&y and stores in  variable z
+s=x-y;// subtract x&y and stores in variable s
+d=x/y;// divide x&y and stores in variable d
+m=x*y;// multilply x&y and stores in variable m
+console.log(`The addition is:${z}`);// display addition with messsage in console
+console.log(`The subtraction is:${s}`);// display subtarction with messsage in console
+console.log(`The division is:${d}`);// display division with messsage in console
+console.log(`The multiplication is:${m}`);// display multiplication with messsage in console
