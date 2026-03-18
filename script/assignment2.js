fc554adaa27a42311ba5dead17b700d3b0c08a27
Anitha-Thunga <ani.cvs@gmail.com>
2026-03-18 11:32:50 +0100

    added javascript to print the reduced price

---
+++ script/assignment2.js
@@ -0,0 +1,13 @@
+let priceTag;
+//Enter the input from user with dollar symbol
+pricetag= prompt(`Enter the price tag with $symbol:`);
+//remove dollar sign from price
+newPriceTag= pricetag.replace("$","");
+console.log(`the price without dollar sign ${newPriceTag}`);
+let price=parseFloat(newPriceTag);
+//reduce the price by 10%
+let discountedPrice=newPriceTag*((100-10)/100);
+//to display the discounted price on browser
+document.getElementById("demo").innerHTML = discountedPrice;
+//to display the discounted price on console with two precisions
+console.log(`Your new price is ${discountedPrice.toFixed(2)}`);
